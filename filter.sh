13cda748f628ce9dbd2ba62ce9a604537ab648c4
Heart-travel <zcgacbc@gmail.com>
2015-10-11 17:14:44 +0800

    add the shell script of filter.sh
    The function of filter.sh:
    Filter the numbers from 000 to 999 from the original data.
    a) If the number has never been shown, then the result file
       will be a record "A".
    b) If the number has only been shown for once, the record
       will be a record "B"
    ...
    ...

---
+++ filter.sh
@@ -0,0 +1,42 @@
+#!/bin/sh
+
+TARGET=0
+TAR_NUM=0
+
+for i in `seq 0 999`
+do
+	TARGET=`printf "%03d" $i`
+	#echo $TARGET
+	TAR_NUM=`grep -nwr $TARGET ./new | wc -l`
+	#echo TAR_NUM=$TAR_NUM
+	if [ $TAR_NUM -eq 0 ]; then
+		TAR_NUM=A
+	elif [ $TAR_NUM -eq 1 ]; then
+		TAR_NUM=B
+	elif [ $TAR_NUM -eq 2 ]; then
+		TAR_NUM=C
+	elif [ $TAR_NUM -eq 3 ]; then
+		TAR_NUM=D
+	elif [ $TAR_NUM -eq 4 ]; then
+		TAR_NUM=E
+	elif [ $TAR_NUM -eq 5 ]; then
+		TAR_NUM=F
+	elif [ $TAR_NUM -eq 6 ]; then
+		TAR_NUM=G
+	elif [ $TAR_NUM -eq 7 ]; then
+		TAR_NUM=H
+	elif [ $TAR_NUM -eq 8 ]; then
+		TAR_NUM=I
+	elif [ $TAR_NUM -eq 9 ]; then
+		TAR_NUM=J
+	elif [ $TAR_NUM -eq 10 ]; then
+		TAR_NUM=K
+	elif [ $TAR_NUM -eq 11 ]; then
+		TAR_NUM=L
+	elif [ $TAR_NUM -eq 12 ]; then
+		TAR_NUM=M
+	elif [ $TAR_NUM -gt 12 ]; then
+		TAR_NUM=Z
+	fi
+	echo $TARGET $TAR_NUM | tee -a new.txt
+done
