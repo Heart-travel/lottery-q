381849484f324469c19d033698578ae93dcb1f87
Heart-travel <zcgacbc@gmail.com>
2016-12-11 16:59:05 +0800

    Update the catch.sh and 2015 database

--- AUTO/catch.sh
+++ AUTO/catch.sh
@@ -1,15 +1,30 @@
 #!/bin/bash 
+# **********************************
+# This file is used for catching the data from web and 
+# filter the format of the data as "2016334 189"
+# **********************************
+
 
 TEMP=../temp/3d.xml
 DEST=../temp/today.txt
+DEST2=../temp/today2.txt
 
 touch $TEMP
+
+# Clean up the files of $DEST, $DEST2
+:> $DEST
+:> $DEST2
+
+# Get the source data and store them in $TEMP.
+# The format of the valid data is as following: 
+# <row expect="2016334" opencode="1,8,9" opentime="2016-12-06 20:32:00"/>
 curl http://f.apiplus.cn/fc3d.xml > $TEMP
 
-#expect="2015302"opencode="1,2,0"
+#expect=`cat $TEMP | grep "expect" | awk -F '"' '{print $2 "\n"  $4}' `
 
-expect=`cat $TEMP | awk ' FS="><"{print $7}' | awk  -F\" '{print $2}'`
-opencode=`cat $TEMP | awk ' FS="><"{print $8}' | awk  -F\" '{print $2}' | awk -F\, '{print $1 $2 $3}'`
+# Store the data in the temp file, wait to be processed.
+# The expect format of the data is "2016334 1,8,9" 
+cat $TEMP | grep "expect" | awk -F '"' '{print $2 "\t"  $4}' >> $DEST
+cat $DEST | awk -F ',' '{print $1$2$3}' >> $DEST2
 
-echo $expect\ \ \ $opencode > $DEST
 rm -r $TEMP
